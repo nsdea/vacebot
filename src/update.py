7ecf5c876df19b99f26b286f772789a9ed958ecc
nsde <xitzfelix123@gmail.com>
2021-06-28 13:39:14 +0200

    test

--- src/update.py
+++ src/update.py
@@ -7,11 +7,11 @@ lokal = open('src/main.py', encoding='utf-8').read()
 
 unterschied = SequenceMatcher(None, online, lokal).ratio() * 100
 
-print(f'Der geupdatete Code online stimmt zu {round(unterschied), 1}% mit deinem Code überein.')
+print(f'Der geupdatete Code online stimmt zu {round(unterschied, 1)}% mit deinem Code überein.')
 
-if 'update' in input('Schreibe "update", um den Bot zu updaten').lower():
+if 'update' in input('Schreibe "update", um den Bot zu updaten: ').lower():
     print('Code wird automatisch geupdatet...')
-    open('src/main.py').write(online)
+    open('src/main.py', 'w', encoding='utf-8').write(online)
     print('Code wurde erfolgreich geupdatet!')
 else:
     print('Update übersprungen.')
